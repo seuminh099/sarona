414486df88528d991c339aceef6e6a8afa991c6f
vechhekaitp11 <63481795+vechhekaitp11@users.noreply.github.com>
2020-04-11 15:21:12 +0700

    Update customerform.php

--- customer_form/customerform.php
+++ customer_form/customerform.php
@@ -1,15 +1,7 @@
 
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 session_start();
-//import class.php.php
-=======
 //import class.php
->>>>>>> 91b89f16e900c19a3dd489fe766acd6804a27ea5
-=======
-//import class.php
->>>>>>> 91b89f16e900c19a3dd489fe766acd6804a27ea5
 	require_once('../inc/class.php');
 //instant object
     $obj = new mycodes;
